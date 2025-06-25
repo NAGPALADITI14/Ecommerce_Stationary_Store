c6e6fefa0a3f133d1426827e6e9a59fb66bd8163
NAGPALADITI14 <138228231+NAGPALADITI14@users.noreply.github.com>
2025-06-26 00:03:22 +0530

    clean code

--- Frontend/src/app/api/auth/login/route.ts
+++ Frontend/src/app/api/auth/login/route.ts
@@ -1,38 +1,3 @@
-// import { NextResponse } from "next/server";
-// import User from "@/models/user";
-// import { dbConnect } from "@/utils/dbconnect";
-// import jwt from "jsonwebtoken";
-// import bcrypt from "bcryptjs";
-
-// const JWT_SECRET = process.env.JWT_SECRET as string;
-
-// export async function POST(req: Request) {
-//   await dbConnect();
-
-//   try {
-//     const { email, password } = await req.json();
-
-//     // Find user
-//     const user = await User.findOne({ email });
-//     if (!user) {
-//       return NextResponse.json({ message: "Invalid credentials" }, { status: 401 });
-//     }
-
-//     // Compare password
-//     const isMatch = await bcrypt.compare(password, user.password);
-//     if (!isMatch) {
-//       return NextResponse.json({ message: "Invalid credentials" }, { status: 401 });
-//     }
-
-//     // Generate JWT Token
-//     const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: "7d" });
-
-//     return NextResponse.json({ token }, { status: 200 });
-//   } catch (error) {
-//     console.error("Login API Error:", error);
-//     return NextResponse.json({ message: "Something went wrong" }, { status: 500 });
-//   }
-// }
 import { NextResponse } from "next/server";
 import User from "@/models/user";
 import { dbConnect } from "@/utils/dbconnect";
--- Frontend/src/app/api/categories/route.ts
+++ Frontend/src/app/api/categories/route.ts
@@ -1,30 +1,3 @@
-// import { dbConnect } from "@/utils/dbconnect";
-// import { Category } from "@/models/Category";
-// import { NextApiRequest, NextApiResponse } from 'next';
-
-// async function getCategories(req: NextApiRequest, res: NextApiResponse) {
-//   await dbConnect();
-
-//   try {
-//     const categories = await Category.find({});
-//     res.status(200).json(categories);
-//   } catch (error) {
-//     res.status(500).json({ error: 'Failed to fetch categories' });
-//   }
-// }
-
-// export async function handler(req: NextApiRequest, res: NextApiResponse) {
-//   if (req.method === 'GET') {
-//     return getCategories(req, res);
-//   } else {
-//     res.status(405).json({ message: 'Method not allowed' });
-//   }
-// }
-
-// // Export the handler function as a named export for each HTTP method
-// export { getCategories };
-
-
 import { dbConnect } from "@/utils/dbconnect";
 import { Category } from "@/models/Category";
 import { NextResponse } from "next/server";
